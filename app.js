d9ca40175260d2b23602db52927cb65999530e3c
Martha Villa Martin <martha.villamartin@gmail.com>
2020-05-27 20:28:30 -0700

    Switches html pages into ejs pages adding partials to those pages and the get requests. Completes all the pages in Spanish

--- app.js
+++ app.js
@@ -6,14 +6,60 @@ const app = express();
 
 app.set("view engine", "ejs")
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
 app.listen(3000, function(){
   console.log("Server started on Port 3000")
 });
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html")
-})
 
-app.get("/contact", function(request, response){
-  response.send("This is the contact page")
-})
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.get('/index.ejs', function(req, res) {
+    res.render('index');
+});
+
+app.get('/crepes.ejs', function(req, res) {
+    res.render('crepes');
+});
+
+app.get('/bebidas.ejs', function(req, res) {
+    res.render('bebidas');
+});
+
+app.get('/contacto.ejs', function(req, res) {
+    res.render('contacto');
+});
+
+app.get('/promocion.ejs', function(req, res) {
+    res.render('promocion');
+});
+
+app.get('/pizzaSimple.ejs', function(req, res) {
+    res.render('pizzaSimple');
+});
+
+app.get('/pizzaQueso.ejs', function(req, res) {
+    res.render('pizzaQueso');
+});
+
+app.get('/pizzaCarne.ejs', function(req, res) {
+    res.render('pizzaCarne');
+});
+
+app.get('/pizzaPersonalizada.ejs', function(req, res) {
+    res.render('pizzaPersonalizada');
+});
+
+app.get('/pizzaPescado.ejs', function(req, res) {
+    res.render('pizzaPescado');
+});
+
+app.get('/pizzaVegetal.ejs', function(req, res) {
+    res.render('pizzaVegetal');
+});
